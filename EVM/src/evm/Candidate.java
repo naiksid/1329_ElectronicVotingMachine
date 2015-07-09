e0d2d7e19634ecf6abfb87a99957ddff4599cd16
naiksid <naiksid@hotmail.com>
2015-07-09 15:42:55 +0530

    V1

---
+++ EVM/src/evm/Candidate.java
@@ -0,0 +1,12 @@
+package evm;
+
+public class Candidate {
+	
+	String Name;
+	int myVotersCount;
+	
+	public Candidate(String name, int myVotersCount) {
+		Name = name;
+		this.myVotersCount = myVotersCount;
+	}
+}
---
+++ EVM/src/evm/ControlUnit.java
@@ -0,0 +1,9 @@
+package evm;
+
+import java.util.*;
+
+public interface ControlUnit {
+	
+	ArrayList <Candidate> candidates = new ArrayList<Candidate>();
+
+}
